306919b335eb95c233ba487dc7121c32414f6332
Héctor Nájera <112099223+HectorNajera@users.noreply.github.com>
2025-05-22 13:02:21 -0700

    push

--- api/handler.js
+++ api/handler.js
@@ -1,6 +1,3 @@
-// const serverless = require('serverless-http');
-// const app = require('./index');
-//
-// module.exports.handler = serverless(app);
 const serverless = require('serverless-http');
+const app = require("./index");
 module.exports.handler = serverless(app);
