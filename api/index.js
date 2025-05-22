c707f7a74f0bbe4f7819ee9873f1f26aa199a2ad
Héctor Nájera <112099223+HectorNajera@users.noreply.github.com>
2025-05-22 13:23:33 -0700

    push

--- api/index.js
+++ api/index.js
@@ -5,14 +5,37 @@ const { instagramGetUrl } = require('instagram-url-direct'); // ✅ fixed here
 const serverless = require("serverless-http");
 
 const app = express();
-// ✅ Setup CORS
-app.use(cors({
-    origin: '*',//'https://igsaver-hoz.web.app', //https://igsaver-hoz.web.app/
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type']
-}));
+// // ✅ Setup CORS
+// app.use(cors({
+//     origin: '*',//'https://igsaver-hoz.web.app', //https://igsaver-hoz.web.app/
+//     methods: ['GET', 'POST', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type']
+// }));
+//
+// app.options('*', cors());  // ✅ Handle preflight
+const whitelist = [
+    '*'
+];
+app.use((req, res, next) => {
+    const origin = req.get('referer');
+    const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+    if (isWhitelisted) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+    }
+    // Pass to next layer of middleware
+    if (req.method === 'OPTIONS') res.sendStatus(200);
+    else next();
+});
 
-app.options('*', cors());  // ✅ Handle preflight
+const setContext = (req, res, next) => {
+    if (!req.context) req.context = {};
+    next();
+};
+app.use(setContext);
+
 app.use(express.json());
 
 app.get("/hello", (req, res) => {
