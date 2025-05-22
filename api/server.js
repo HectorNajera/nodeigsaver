9b1ad30d24bce57c048c0e8da4458c2cef5b3237
Héctor Nájera <112099223+HectorNajera@users.noreply.github.com>
2025-05-22 11:15:55 -0700

    push changes

--- api/server.js
+++ api/server.js
@@ -4,11 +4,14 @@ const bodyParser = require('body-parser');
 const { instagramGetUrl } = require('instagram-url-direct'); // ✅ fixed here
 const serverless = require("serverless-http");
 
+// const app = express();
+// const PORT = 3000;
+//
+// app.use(cors());
+// app.use(bodyParser.json());
 const app = express();
-const PORT = 3000;
-
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get("/api/hello", (req, res) => {
     res.json({ message: "Hello from Express on Vercel!" });
