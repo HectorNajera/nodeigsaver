f4d0165629109453b01fcd671820ac763c05b447
Héctor Nájera <112099223+HectorNajera@users.noreply.github.com>
2025-05-22 18:33:52 -0700

    push new changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.options('*', cors());
 app.use(express.json());
 
 app.get('/api/hello', (req, res) => {
-    res.json({ message: 'Hello from Express on Vercel!' });
+    res.json({ message: 'Hello from Express on Vercel!!!' });
 });
 
 app.post('/api/fetch-instagram', async (req, res) => {
